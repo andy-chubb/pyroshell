dae2ee35c8be246401fb79a3ff619fb2ce604202
Andy Chubb <andy@wearecube3.com>
2014-01-26 15:29:39 +0000

    initial commit

---
+++ theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Theme_Pyroshell extends Theme
+{
+    public $name = 'Equuleus';
+    public $author = 'Andrew Chubb';
+    public $author_website = 'http://www.andychubb.com';
+    //public $website = 'http://example.com/themes/foo';
+    public $description = 'Barebones Responsive Theme, built on Fireshell and CSSWizadry Grids. SASS preprocessing and JavaScript optimisation with Grunt.js. Modular SASS and BEM grid courtesy of CSSWizadry';
+    public $version = '0.5';
+}
+ 
+ /* End of file theme.php */
